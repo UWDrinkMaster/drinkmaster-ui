a4e09c0c0322e71d2b948fdfdb4eafd8c7c416f4
Yiwen Wu <y657wu@uwaterloo.ca>
2023-07-24 16:25:27 -0400

    add profile view for demo

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,7 +1,14 @@
 <template>
-<div>
-  profile
-  <i class="el-icon-switch-button" @click="logout"></i>
+  <div>
+
+    <div style="width: 100%; height: 100%; position: relative; background: white">
+      <div style="width: 257px; left: 30%; top: 187px; position: absolute; color: #09101D; font-size: 19px; font-weight: 600; line-height: 20px; word-wrap: break-word">Email: {{this.$store.getters.getUser.username}}</div>
+      <img style="width: 116px; height: 111px; left: 37%; top: 60px; position: absolute" src="../../assets/img/user_icon.png" />
+
+      <div style="padding-left: 20px; padding-right: 20px; padding-top: 8px; padding-bottom: 8px; left: 37%; top: 264px; position: absolute; background: linear-gradient(180deg, #F9E8A8 0%, #F4C317 91%); border-radius: 24px; justify-content: flex-start; align-items: center; gap: 8px; display: inline-flex">
+        <div style="color: white; font-size: 18px; font-family: Source Sans Pro; font-weight: 600; line-height: 28px; word-wrap: break-word" @click="logout">Sign Out</div>
+      </div>
+    </div>
 
 </div>
 </template>
@@ -26,3 +33,12 @@
       }
     };
 </script>
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center; /* Center vertically */
+    height: 100vh;
+  }
+
+</style>
--- src/components/user/common/TopBar.vue
+++ src/components/user/common/TopBar.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <el-menu :default-active="this.$router.path" class="el-menu-demo topbartext" mode="horizontal" @select="handleSelect" router>
-      <el-menu-item index="/user/menu">Drink Menu</el-menu-item>
-      <!--<el-submenu index="/user/home">-->
-      <!--  <template slot="title">Drink Menu</template>-->
-      <!--  <el-menu-item index="1-1">Alcohol</el-menu-item>-->
-      <!--  <el-menu-item index="1-2">Non-Alcohol</el-menu-item>-->
-      <!--  <el-menu-item index="1-3">Create Your Own Drink</el-menu-item>-->
-      <!--</el-submenu>-->
+    <el-menu :default-active="activeIndex" class="el-menu-demo topbartext topbar" mode="horizontal" @select="handleSelect" background-color="rgb(84, 92, 100)" text-color="rgb(255, 255, 255)" active-text-color="rgb(255, 208, 75)"  router>
+<!--      <el-menu-item  class ="menuitem" index="/user/menu">Drink Menu</el-menu-item>-->
+      <el-submenu index= "2" class ="topbartext">
+        <template slot="title">Drink Menu</template>
+        <el-menu-item index="/user/menu">Recommended</el-menu-item>
+        <el-menu-item index="/user/menu">Non-Alcohol</el-menu-item>
+        <el-menu-item index="/user/menu">Alcohol</el-menu-item>
+        <el-menu-item index="/user/menu">Create Your Own Drink</el-menu-item>
+      </el-submenu>
       <el-menu-item index="/user/order"> Order History</el-menu-item>
-      <el-menu-item index="/user/profile"> User Info</el-menu-item>
+      <el-menu-item index="/user/profile"> Profile</el-menu-item>
     </el-menu>
     <div class="line"></div>
   </div>
@@ -20,7 +21,7 @@
     name: "TopBar",
     data() {
       return {
-        activeIndex: '1'
+        activeIndex: '/user/menu'
       };
     },
     methods: {
@@ -33,7 +34,8 @@
   }
 </script>
 <style>
-.topbartext{
-  font: 600 16px "Manrope", sans-serif;
-}
+  .topbartext {
+    font: 600 16px "Manrope", sans-serif;
+  }
+
 </style>
